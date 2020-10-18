b839a16498a2f4869478dd620fc4257cc7cd8a45
Jiyun <baejiyun01@gmail.com>
2020-10-18 17:40:53 +0900

    updating changed js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 (() => {
+    console.log("fired");
 
+    let button = document.querySelector("#button");
+
+    function showHideLightbox() {
+        lightBox.classList.toggle('show-lightbox');
+    }
+
+    button.addEventListener("click", hamburgerMenu, false);
+
+    sigils.forEach(sigil => sigil.addEventListener("click", showHideLightbox));
 })();
